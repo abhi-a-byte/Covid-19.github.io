aa675d1e73082962b77cdd3c8411a56bec51acbf
abhi-a-byte <68660068+abhi-a-byte@users.noreply.github.com>
2020-07-30 07:35:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+const countries =document.querySelector('datalist');
+const search =document.querySelector('#srch');
+const date =document.querySelector('#date');
+const nameCountry =document.querySelector('#name-country');
+const confirmed =document.querySelector('.confirmed');
+const deaths =document.querySelector('.deaths');
+const recovered =document.querySelector('.recovered');
+const chart =document.querySelector('.chart');
+
+let dataChart=[];
+
+const API_URL= "https://api.covid19api.com/summary";
+
+async function covid(country){
+    countries.innerHTML='<option value="World">World</option>';
+    resetValue(confirmed);
+    resetValue(deaths);
+    resetValue(recovered);
+
+    const res = await fetch(API_URL);
+    console.log(res);
+    const data=await res.json();
+    console.log(data);
+    if(res.status === 4 || res.status=== 200){
+        date.textContent= new Date(data.Date).toDateString();
+        console.log(data.Global);
+       // const {TotalConfirmed,TotalDeaths,TotalRecovered,NewConfirmed,NewDeaths}
+
+        if(country ==='' || country==='world'){
+            const {TotalConfirmed,TotalDeaths,TotalRecovered,NewConfirmed,NewDeaths,NewRecovered}=data.Global;
+            total(TotalConfirmed,TotalDeaths,TotalRecovered);
+               new newUpdate(NewConfirmed,NewDeaths,NewRecovered);
+
+
+               nameCountry.textContent="The World";
+               dataChart=[TotalConfirmed,TotalDeaths,TotalRecovered];
+
+        };
+
+
+        data.Countries.forEach(item =>{
+
+            const option=document.createElement('option');
+            option.value=item.Country;
+            option.textContent=item.Country;
+            countries.appendChild(option);
+
+            if(country===item.Country){
+               total(item.TotalConfirmed,item.TotalDeaths,item.TotalRecovered);
+               new newUpdate(item.NewConfirmed,item.NewDeaths,item.NewRecovered);
+
+
+               nameCountry.textContent= item.Country;
+               dataChart=[item.TotalConfirmed,item.TotalDeaths,item.TotalRecovered];
+
+            }
+            
+        });
+
+        drawChart(dataChart);
+        
+
+
+
+    }
+    else{
+        chart.innerHTML= '<h2>Loading......</h2>';
+    }
+}
+const speed=100;
+function counting(target,element){
+    const inc= target/speed;
+    const count= +element.textContent;
+    if(count<target){
+        element.textContent= Math.ceil(count+inc);
+        setTimeout(()=>{
+            counting(target,element)
+        },1)
+    }
+    else{
+        //Total Confirmed
+        element.textContent=target;
+
+    }
+
+};
+
+function total(Confirmed,Deaths,Recovered){
+    
+        counting(Confirmed,confirmed.children[1]);       
+        //Total Deaths
+        counting(Deaths,deaths.children[1]);
+        //Total recovered
+        counting(Recovered,recovered.children[1]);
+        
+        
+};
+
+function newUpdate(Confirmed,Deaths,Recovered){
+    //New Confirmed
+    counting(Confirmed,confirmed.children[2]);  
+    
+                
+        //New Deaths
+        counting(Deaths,deaths.children[2]);  
+        
+        
+        //New recovered
+        counting(Recovered,recovered.children[2]);  
+        
+        
+};
+
+
+function resetValue(element){
+    element.children[1].textContent=0;
+    element.children[2].textContent=0;
+};
+
+function drawChart(data){
+    chart.innerHTML='';
+    const ctx=document.createElement('canvas');
+    chart.appendChild(ctx);
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Total Confirmed', 'Total Deaths', 'Total Recovered', ],
+            datasets: [{
+                label: nameCountry.textContent,
+                data: data,
+                backgroundColor: ['crimson','black','green']
+
+            }]
+        },
+        options: {}
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+covid(search.value);
+const btnSearch=document.querySelector('button');
+btnSearch.addEventListener('click',(e)=>{
+    e.preventDefault();
+    covid(search.value);
+    console.log(search.value);
+    search.value='';
+})
+
+
+
+   
+   
